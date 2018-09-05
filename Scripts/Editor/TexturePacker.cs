c9ce6c56fbc1bc73aab662ea79f068a84af6a816
Jangkyu Seo <jangkyu@unity3d.com>
2018-09-05 17:37:29 +0900

    Store TextureAtlas which is created by TexturePacker.

--- Scripts/Editor/TexturePacker.cs
+++ Scripts/Editor/TexturePacker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -207,7 +208,12 @@ namespace UnityEditor.Experimental.AutoLOD
 
             textureAtlas.Apply();
 
+            var name = Path.GetRandomFileName();
+            textureAtlas = SaveTexture(textureAtlas, name);
+
             atlas.textureAtlas = textureAtlas;
+
+            SaveUniqueAtlasAsset(atlas, name);
 
             return atlas;
         }
@@ -222,6 +228,11 @@ namespace UnityEditor.Experimental.AutoLOD
             {
                 textureImporter.isReadable = true;
                 textureImporter.SaveAndReimport();
+            }
+            else if (!assetImporter)
+            {
+                // In-memory textures need to be saved to disk in order to be referenced by the texture atlas
+                SaveUniqueAtlasAsset(texture, Path.GetRandomFileName());
             }
 
             int sideSize = Math.Max(texture.width, texture.height);
@@ -270,6 +281,42 @@ namespace UnityEditor.Experimental.AutoLOD
                 minTextureCount = minTextureCount * 4;
 
             return minTextureCount;
+        }
+
+        static Texture2D SaveTexture(Texture2D texture, string name)
+        {
+            var path = "AutoLOD/Generated/Atlases/" + name;
+            path = Path.ChangeExtension(path, "PNG");
+
+            var assetPath = "Assets/" + path;
+            var dataPath = Application.dataPath + "/" + path;
+
+            var dirPath = Path.GetDirectoryName(dataPath);
+            if (Directory.Exists(dirPath) == false)
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            byte[] binary = texture.EncodeToPNG();
+            File.WriteAllBytes(dataPath,binary);
+
+            AssetDatabase.ImportAsset(assetPath);
+            return AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
+
+
+        }
+
+        static void SaveUniqueAtlasAsset(UnityEngine.Object asset, string name)
+        {
+            var directory = "Assets/AutoLOD/Generated/Atlases/";
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            var path = directory + name;
+            path = Path.ChangeExtension(path, "asset");
+            AssetDatabase.CreateAsset(asset, path);
+
+
         }
 
        
